1788c97cf7618a919e9e78499748bc192f9139d5
Michael Brandt <MichaelMBPro@Michaels-MacBook-Pro.local>
2019-02-21 02:16:45 +0100

    Add Semantic UI Make Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
+import HeaderNav from "./Containers/HeaderNav/HeaderNav";
 
 class App extends Component {
   render() {
-    return <div>Hello World</div>;
+    return <HeaderNav />;
   }
 }
 
